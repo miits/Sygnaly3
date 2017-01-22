0ab4fa5e894a5b6a9f147df4cf78351997fe6478
miits <mikolaj.szal@gmail.com>
2017-01-22 20:16:45 +0100

    lowpass filter update

--- sygnaly3.py
+++ sygnaly3.py
@@ -33,56 +33,34 @@ def lowpass(voice, rate, threshold):
     return lowpass_voice
 
 
-rate, voice = read_file("train/003_K.wav")
-voice = cut_signal_length(voice, rate, 3)
-
-# ln = len(voice)
-# cor = []
-#
-# for i in range(0, ln, 10):
-#     end = ln - 1 - i
-#     begin = ln - end
-#     cor.append(sts.pearsonr(voice[0:end], voice[begin::])[0])
-#
-# cor = cor[250::]
-#
-# i_max_1 = cor.index(max(cor))
-# #i_max_1 = i_max_1[0]
-#
-# diff = 100
-# if (i_max_1 - diff) < 0:
-#     i_max_2 = cor.index(max(cor[(i_max_1 + diff)::]))
-#     #i_max_2 = i_max_2[0] #+ i_max_1 + diff
-# elif (i_max_1 + diff) > ln:
-#     i_max_2 = np.where(cor == max(cor[0:(i_max_1 - diff)]))
-#     #i_max_2 = i_max_2[0]
-# else:
-#     i_max_21 = cor.index(max(cor[0:(i_max_1 - diff)]))
-#     #i_max_21 = i_max_21[0]
-#     i_max_22 = cor.index(max(cor[(i_max_1 + diff)::]))
-#     #i_max_22 = i_max_22[0] #+ i_max_1 + diff
-#     i_max_2 = i_max_21
-#     if cor[i_max_22] > cor[i_max_21]:
-#         i_max_2 = i_max_22
-#
-#
-# T = abs(i_max_1 - i_max_2) * 10 / rate
-# f = 1/T
-
-#print(f)
-filtered_voice = lowpass(voice, rate, 500)
-auto_correlated = autocorr(filtered_voice)
-maximum = sig.argrelextrema(auto_correlated, np.greater)
-T = abs(maximum[0][0] - maximum[0][1]) / rate
-f = 1/T
-print(f)
-plt.subplot(311)
-plt.plot(voice)
-plt.subplot(312)
-plt.plot(filtered_voice)
-plt.subplot(313)
-plt.plot(auto_correlated)
-plt.show()
+def get_key(i):
+    return voice[i]
 
 
+def main():
+    global voice
+    rate, voice = read_file("train/003_K.wav")
+
+    #voice = cut_signal_length(voice, rate, 1)
+
+
+    filtered_voice = lowpass(voice, rate, 1000)
+    auto_correlated = autocorr(filtered_voice)
+    maximum = sig.argrelextrema(auto_correlated, np.greater)
+    sorted_maximum = sorted(maximum[0], key=get_key, reverse=True)
+    T = abs(sorted_maximum[0] - sorted_maximum[1]) / rate
+    #T = abs(maximum[0][0] - maximum[0][2]) / rate
+    f = 1/T
+    print(f)
+    print(maximum[0][0:9])
+    print(sorted_maximum[0:9])
+    plt.subplot(311)
+    plt.plot(voice)
+    plt.subplot(312)
+    plt.plot(filtered_voice)
+    plt.subplot(313)
+    plt.plot(auto_correlated)
+    plt.show()
+
+main()
 
